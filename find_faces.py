b2f6334cf6a56e4d1a297a6f96ae131f4dcff558
Artem Shuliatev <34714886+AEShulyatev@users.noreply.github.com>
2018-09-19 22:48:15 +0300

    face features detection

---
+++ find_faces.py
@@ -0,0 +1,24 @@
+import cv2
+import dlib
+
+sp = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+detector = dlib.get_frontal_face_detector()
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+win1 = dlib.image_window()
+
+
+while True:
+    ret, img = cap.read()
+    img = cv2.flip(img, 1)
+    win1.set_image(img)
+    dets = detector(img, 0)
+    for k, d in enumerate(dets):
+        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
+            k, d.left(), d.top(), d.right(), d.bottom()))
+        shape = sp(img, d)
+        win1.add_overlay(d)
+        win1.add_overlay(shape)
+    cv2.waitKey(100)
+    win1.clear_overlay()
