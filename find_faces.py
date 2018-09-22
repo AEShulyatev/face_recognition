17ef3c951d1fe2b7654365d15c97f59c55e1e1d3
AEShulyatev <artemsh05@mail.ru>
2018-09-23 01:31:04 +0300

    increase distance of finding faces

--- find_faces.py
+++ find_faces.py
@@ -1,5 +1,6 @@
 import cv2
 import dlib
+from scipy.misc import imresize
 
 sp = dlib.shape_predictor('datasets\\shape_predictor_68_face_landmarks.dat')
 detector = dlib.get_frontal_face_detector()
@@ -12,6 +13,7 @@ win1 = dlib.image_window()
 while True:
     ret, img = cap.read()
     img = cv2.flip(img, 1)
+    img = imresize(img, (img.shape[0] * 2, img.shape[1] * 2))
     win1.set_image(img)
     dets = detector(img, 0)
     for k, d in enumerate(dets):
